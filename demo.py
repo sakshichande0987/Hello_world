523aa1fb59530ff59841850e946abba53cb06a66
sakshichande0987 <141729965+sakshichande0987@users.noreply.github.com>
2023-08-08 17:16:08 +0530

    Update demo.py
    branch2

--- demo.py
+++ demo.py
@@ -1,6 +1,6 @@
-#This is code will print hello world message 
+This is code will print hello world message 
 print("Hello world")
-#This is code will print hello world message 
+#This is code will pri#nt hello world message 
 print("Hello world")
 his is code will print hello world message 
 print("Hello world")
